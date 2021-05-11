19ddf7b354ac2713b58e8a62158a5e107b7a106f
George Ofogba <ofogbageorge3@gmail.com>
2021-05-11 16:53:07 +0100

    sidebar component created

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Sidebar from '../sidebar/Sidebar'
 import './Home.css'
 
 const Home = () => {
     return (
         <div className='home'>
-            
+            <Sidebar />
         </div>
     )
 }
